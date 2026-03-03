c68afb56408b26e02f4b0037e9c40a39ce5aad27
Fabian Bell <fabian.bell@barrabytes.com>
2026-03-03 09:35:38 +0100

    Expose additional agent registry and router utilities in the public API

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph_dynamodb_checkpoint import DynamoDBSaver
 
+from distributed_a2a import load_router, registry_heart_beat, AgentRegistryClient, McpRegistryClient
 from distributed_a2a.registry_server import load_registry, AgentRegistryLookup, McpRegistryLookup, DynamoDbAgentRegistryLookup, DynamoDbMcpRegistryLookup, InMemoryMcpRegistry, InMemoryAgentRegistry
 from distributed_a2a.client import RoutingA2AClient
 from distributed_a2a.server import load_app
@@ -9,6 +10,7 @@ from distributed_a2a.model import AgentConfig, SkillConfig, RegistryItemConfig, 
 
 __all__ = [
     "load_app",
+    "load_router",
     "RoutingA2AClient",
     "load_registry",
     "AgentRegistryLookup",
@@ -24,6 +26,9 @@ __all__ = [
     "AgentItem",
     "RouterItem",
     "RouterConfig",
+    "registry_heart_beat",
+    "AgentRegistryClient",
+    "McpRegistryClient",
     "MemorySaver",
     "DynamoDBSaver",
     "InMemoryAgentRegistry",
